ba53fb6a98750cf44aee10ac65196c0a32573dfc
Kevin <stradtkt22@gmail.com>
2019-06-29 17:02:14 -0400

    Added middleware to the app

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 connectDB();
+app.use(express.json({extended: false}));
 app.get('/', (req,res) => {
     res.send('API Running');
 });
